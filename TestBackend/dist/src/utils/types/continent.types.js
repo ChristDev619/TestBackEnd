9767cab7d0ee07297eef8180689c8c292d773d84
Christian Chindy <christian_chindy@hotmail.com>
2025-02-14 20:49:38 +0200

    Stop tracking dist, node_modules, and build folders

--- TestBackend/dist/src/utils/types/continent.types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=continent.types.js.map
--- TestBackend/dist/utils/types/discoverySource.types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=discoverySource.types.js.map
